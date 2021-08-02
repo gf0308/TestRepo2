9e9a592400e98d2c9cadfb7b0f91242037317cfa
Jongbin_Kim <kjbk0308@gmail.com>
2021-08-02 18:25:46 +0900

    직렬화 프로젝트 첫번째 커밋

---
+++ src/main/java/com/project/ProjectSerializationApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSerializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSerializationApplication.class, args);
+	}
+
+}
